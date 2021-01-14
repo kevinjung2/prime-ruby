6ce22bca3b741f75c480cea6302990899d9bfa9f
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 22:29:30 -0500

    commit tree 7db6150416c9a4cda93dec5e8531f46010f203f6

--- prime.rb
+++ prime.rb
@@ -1,1 +1,42 @@
-# Add  code here!
+# def prime?(int)
+#   if int == 2
+#     true
+#   elsif int >= 2
+#     range = (2..int).to_a
+#     i = 0
+#     crossed = []
+#     while i < range.length
+#       if crossed.include?(range[i]) == false
+#         range.each.with_index do |num,index|
+#           if index > i
+#             crossed << num if num % range[i] == 0 && crossed.include?(num) == false
+#           end
+#         end
+#       end
+#       i += 1
+#     end
+#     if crossed.include?(int)
+#       false
+#     else
+#       true
+#     end
+#   else
+#     false
+#   end
+# end
+
+def prime?(int)
+  if int >= 2
+    range = (2..int**0.5).to_a
+    # range.delete(int)
+    puts "#{range}"
+    range.each { |num| return false if int % num == 0 }
+    true
+  else
+    false
+  end
+end
+
+
+answer = prime?(10)
+puts answer
